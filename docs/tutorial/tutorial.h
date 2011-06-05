3fb1dd44dcb9388469956c298e89ed61baaa0399
Gergely Nagy <algernon@balabit.hu>
2011-06-05 11:30:29 +0200

    docs: Beginnings of a tutorial.
    Factored the API doc links out of src/mongo.h, and moved them into
    docs/api.h instead.
    
    Furthermore, started writing a Tutorial: for now, it covers BSON
    building, but the infrastructure is ready for more.

---
+++ docs/tutorial/tutorial.h
@@ -0,0 +1,13 @@
+/** @page tutorial Tutorial
+ *
+ * These pages will attempt to guide one through the libmongo-client
+ * library, starting from the basic BSON building blocks, through the
+ * low level wire protocol API, until the highest level synchronous
+ * API.
+ *
+ * The documentation assumes a reasonable amount of C knowledge, and
+ * basic familiarity with MongoDB concepts.
+ *
+ * Contents:
+ *   - @subpage tut_bson
+ */
