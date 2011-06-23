b85572a862836f34fc444be2132250f6bdb08f1a
Gergely Nagy <algernon@balabit.hu>
2011-06-23 21:23:14 +0200

    docs: Added compile instructions to the tutorial.

--- docs/tutorial/tutorial.h
+++ docs/tutorial/tutorial.h
@@ -8,6 +8,15 @@
  * The documentation assumes a reasonable amount of C knowledge, and
  * basic familiarity with MongoDB concepts.
  *
+ * The example programs can be found in the @c docs/tut/examples
+ * directory in the source tree, along with a Makefile. Would one want
+ * to compile the examples, or modified versions of them by hand, then
+ * the following command should work:
+ *
+ * @verbatim
+$ cc $(pkg-config --cflags --libs libmongo-client) tut_bson_build.c -o tut_bson_build
+@endverbatim
+ *
  * Contents:
  *   - @subpage tut_bson
  *     - @ref tut_bson_build
