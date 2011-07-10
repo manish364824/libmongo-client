bba61a0f8253d3a267e3ec42fe1f30b5a58348c4
Gergely Nagy <algernon@balabit.hu>
2011-07-10 13:55:10 +0200

    tests: Fix the sync_gridfs_new unit test.
    The unit test was using the test.fs. prefix when it should've used
    config.gfs_prefix.

--- tests/unit/mongo/sync-gridfs/sync_gridfs_new.c
+++ tests/unit/mongo/sync-gridfs/sync_gridfs_new.c
@@ -8,6 +8,7 @@ test_mongo_sync_gridfs_new (void)
 {
   mongo_sync_connection *conn;
   mongo_sync_gridfs *gfs;
+  gchar *f, *c;
 
   conn = test_make_fake_sync_conn (4, TRUE);
 
@@ -27,21 +28,26 @@ test_mongo_sync_gridfs_new (void)
 
   begin_network_tests (4);
 
+  f = g_strconcat (config.gfs_prefix, ".files", NULL);
+  c = g_strconcat (config.gfs_prefix, ".chunks", NULL);
+
   conn = mongo_sync_connect (config.primary_host, config.primary_port, FALSE);
 
-  gfs = mongo_sync_gridfs_new (conn, "test.fs");
+  gfs = mongo_sync_gridfs_new (conn, config.gfs_prefix);
   ok (gfs != NULL,
       "mongo_sync_gridfs_new() works");
-  is (gfs->ns.prefix, "test.fs",
+  is (gfs->ns.prefix, config.gfs_prefix,
       "The namespace prefix is as specified");
-  is (gfs->ns.files, "test.fs.files",
+  is (gfs->ns.files, f,
       "The files namespace is correct");
-  is (gfs->ns.chunks, "test.fs.chunks",
+  is (gfs->ns.chunks, c,
       "The chunks namespace is correct");
   mongo_sync_gridfs_free (gfs, FALSE);
 
   mongo_sync_disconnect (conn);
 
+  g_free (f);
+  g_free (c);
   end_network_tests ();
 }
 
