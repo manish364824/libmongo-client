146f63f69d367053df3153d11549f2e66f6028f3
Gergely Nagy <algernon@balabit.hu>
2012-04-16 13:52:45 +0200

    Bump copyright years.
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- src/sync-gridfs.h
+++ src/sync-gridfs.h
@@ -1,5 +1,5 @@
 /* sync-gridfs.h - libmong-client GridFS API
- * Copyright 2011 Gergely Nagy <algernon@balabit.hu>
+ * Copyright 2011, 2012 Gergely Nagy <algernon@balabit.hu>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
