18f9f7721251631ddd9ac417612f6f041c94292f
Gergely Nagy <algernon@balabit.hu>
2012-11-10 13:56:48 +0100

    Update docs for 0.1.6.1 + fixes
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- search/variables_73.js
+++ search/variables_73.js
@@ -6,5 +6,6 @@ var searchData=
   ['slaveok',['slaveok',['../struct__mongo__sync__connection.html#a61e9989798c49259a04a98acc79dd6a0',1,'_mongo_sync_connection']]],
   ['slaves',['slaves',['../struct__mongo__sync__pool.html#a7ff5aeadc80988cc806023aa50ada318',1,'_mongo_sync_pool']]],
   ['start',['start',['../structmongo__reply__packet__header.html#a440e2835c6bec775cdd52560d0b2e9cc',1,'mongo_reply_packet_header']]],
+  ['start_5foffset',['start_offset',['../struct__mongo__sync__gridfs__stream.html#aed122265ab36a049348072f43c30c871',1,'_mongo_sync_gridfs_stream']]],
   ['super',['super',['../struct__mongo__sync__connection.html#acac65d890f83b492e16af2003fc4dce0',1,'_mongo_sync_connection::super()'],['../struct__mongo__sync__pool__connection.html#a71ec5171bfc47a74eb043ac5d6fc4a6e',1,'_mongo_sync_pool_connection::super()']]]
 ];
