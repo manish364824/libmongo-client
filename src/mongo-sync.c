83a32de30228e1956a8dc801e8057629a52b7e63
Gergely Nagy <algernon@balabit.hu>
2013-12-10 11:58:11 +0100

    sync: Automatically re-authenticate on reconnect
    If we were authenticated, via mongo_sync_cmd_authenticate(), then
    reauthenticate with the same credentials on reconnect. The credentials
    are stored in an mlock()'ed area for safety, and are zeroed out before
    freeing them up.
    
    Signed-off-by: Gergely Nagy <algernon@balabit.hu>

--- src/mongo-sync.c
+++ src/mongo-sync.c
@@ -26,6 +26,7 @@
 #include <errno.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/mman.h>
 
 mongo_sync_connection *
 mongo_sync_connect (const gchar *address, gint port,
@@ -47,6 +48,10 @@ mongo_sync_connect (const gchar *address, gint port,
   s->rs.primary = NULL;
   s->last_error = NULL;
   s->max_insert_size = MONGO_SYNC_DEFAULT_MAX_INSERT_SIZE;
+
+  s->auth.db = NULL;
+  s->auth.user = NULL;
+  s->auth.pw = NULL;
 
   return s;
 }
@@ -182,6 +187,10 @@ mongo_sync_reconnect (mongo_sync_connection *conn,
               e = errno;
               _mongo_sync_connect_replace (conn, nc);
               errno = e;
+              if (conn->auth.db && conn->auth.user && conn->auth.pw)
+                mongo_sync_cmd_authenticate (conn, conn->auth.db,
+                                             conn->auth.user,
+                                             conn->auth.pw);
               return conn;
             }
         }
@@ -207,6 +216,12 @@ mongo_sync_reconnect (mongo_sync_connection *conn,
       e = errno;
       _mongo_sync_connect_replace (conn, nc);
       errno = e;
+
+      if (conn->auth.db && conn->auth.user && conn->auth.pw)
+        mongo_sync_cmd_authenticate (conn, conn->auth.db,
+                                     conn->auth.user,
+                                     conn->auth.pw);
+
       return conn;
     }
 
@@ -229,11 +244,33 @@ mongo_sync_reconnect (mongo_sync_connection *conn,
       e = errno;
       _mongo_sync_connect_replace (conn, nc);
       errno = e;
+
+      if (conn->auth.db && conn->auth.user && conn->auth.pw)
+        mongo_sync_cmd_authenticate (conn, conn->auth.db,
+                                     conn->auth.user,
+                                     conn->auth.pw);
+
       return conn;
     }
 
   errno = EHOSTUNREACH;
   return NULL;
+}
+
+static void
+_mongo_auth_prop_destroy (gchar **prop)
+{
+  size_t l;
+
+  if (!prop || !*prop)
+    return;
+
+  l = strlen (*prop);
+  memset (*prop, 0, l);
+  munlock (*prop, l);
+  g_free (*prop);
+
+  *prop = NULL;
 }
 
 void
@@ -243,6 +280,10 @@ mongo_sync_disconnect (mongo_sync_connection *conn)
 
   if (!conn)
     return;
+
+  _mongo_auth_prop_destroy (&conn->auth.db);
+  _mongo_auth_prop_destroy (&conn->auth.user);
+  _mongo_auth_prop_destroy (&conn->auth.pw);
 
   g_free (conn->rs.primary);
   g_free (conn->last_error);
@@ -1543,6 +1584,7 @@ mongo_sync_cmd_authenticate (mongo_sync_connection *conn,
   GChecksum *chk;
   gchar *hex_digest;
   const gchar *digest;
+  gchar *ndb, *nuser, *npw;
 
   if (!db || !user || !pw)
     {
@@ -1627,6 +1669,21 @@ mongo_sync_cmd_authenticate (mongo_sync_connection *conn,
     }
   bson_free (b);
   mongo_wire_packet_free (p);
+
+  ndb = g_strdup (db);
+  _mongo_auth_prop_destroy (&conn->auth.db);
+  conn->auth.db = ndb;
+  mlock (conn->auth.db, strlen (ndb));
+
+  nuser = g_strdup (user);
+  _mongo_auth_prop_destroy (&conn->auth.user);
+  conn->auth.user = nuser;
+  mlock (conn->auth.user, strlen (nuser));
+
+  npw = g_strdup (pw);
+  _mongo_auth_prop_destroy (&conn->auth.pw);
+  conn->auth.pw = npw;
+  mlock (conn->auth.pw, strlen (npw));
 
   return TRUE;
 }
